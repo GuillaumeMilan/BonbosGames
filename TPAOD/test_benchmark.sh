636a92c6551fde989408f30287ced884f735f216
Geoffrey Kouvtanovitch <geoffrey.kouvtanovitch@ensimag.grenoble-inp.fr>
2017-04-13 17:52:01 +0200

    ajout d'une boucle de test (commentée) pour la récupération des data + script de test + GUILLAUME EST UN BONOBO AVERAGE

---
+++ TPAOD/test_benchmark.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+#compilation avec gcc du fichier
+gcc ./src/compileBST.c
+
+for i in ./benchmarks/benchmark*.in
+do
+    nom_fichier="${i##\.\/}"
+    echo $nom_fichier
+    nb_element="$(grep -o [-]*[0-9]* $i | wc -l)"
+    echo $i
+    echo $nb_element
+    ./src/a.out $nb_element $nom_fichier
+
+done
