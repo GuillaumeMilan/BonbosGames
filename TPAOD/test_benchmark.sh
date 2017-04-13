34f815e86d4e7d07b75d372da626fd083140f4b0
Geoffrey Kouvtanovitch <geoffrey.kouvtanovitch@ensimag.grenoble-inp.fr>
2017-04-13 19:24:24 +0200

    BIM

--- TPAOD/test_benchmark.sh
+++ TPAOD/test_benchmark.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 
-#compilation avec gcc du fichier
+# Pour les outputs en couleurs
+GREEN='\033[0;32m'
+RED='\033[0;31m'
+
+# Compilation avec gcc du fichier
 gcc ./src/compileBST.c
 cp a.out ./bin/a.out
 rm a.out
@@ -10,12 +14,23 @@ do
     nom_fichier="${i##\.\/}"
     echo "Fichier testé = $nom_fichier"
     nb_element="$(grep -o [-]*[0-9]* $i | wc -l)"
+    # Récupération de la sortie de notre programme
     sortie="$(./bin/a.out $nb_element $nom_fichier)"
+
+    # A commenter dans la version finale
     echo "$sortie"
     
     nom_fichier_sortie="${nom_fichier%\.in}"
     nom_fichier_sortie="$nom_fichier_sortie".out
-    #tail -n +2 $mon_fichier_sortie
+    # Recuperation des bonnes lignes dans le fichier
+    sortie_theorique="$(tail -n +2 $mon_fichier_sortie)"
 
+    # On compare les deux sorties
+    if [ "$sortie_theorique" = "$sortie" ]
+    then
+	echo -e "${GREEN}Passed${NC}"
+    else
+	echo -e "${RED}Failed${NC}"
+    fi
 
 done
